b81171ca7cf38d371a15d2cfb2f9a437755c24c6
KingstonKrunal <krunalshah661@gmail.com>
2021-11-21 00:55:52 -0500

    Interface VIPPrivilege added, and it is deleted from GoldMember class.

--- Assignment-3/GoldMember.java
+++ Assignment-3/GoldMember.java
@@ -1,13 +1,5 @@
 import java.util.Scanner;
 
-interface VIPPrivilege {
-    public final int PASSWORD = 1234; // initialization of password and set it to constant 1234
-
-    public void accessVIPLounge(); // defined a method which will be used in GoldMember class
-
-    public String purchaseAlcohol(); // defined a method which will be used in GoldMember class
-}
-
 public class GoldMember extends SilverMember implements VIPPrivilege {
     private boolean validCredentials; // define a boolean variable for checking valid credentials
 
---
+++ Assignment-3/VIPPrivilege.java
@@ -0,0 +1,7 @@
+interface VIPPrivilege {
+    public final int PASSWORD = 1234; // initialization of password and set it to constant 1234
+
+    public void accessVIPLounge(); // defined a method which will be used in GoldMember class
+
+    public String purchaseAlcohol(); // defined a method which will be used in GoldMember class
+}
